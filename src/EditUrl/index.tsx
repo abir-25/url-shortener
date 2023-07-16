a1c7b5c8f641a64c51f028b623bbecf823bfaffe
abir-25 <abirbgc25@gmail.com>
2023-07-16 21:11:04 +0600

    Project setup completed

---
+++ src/EditUrl/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function EditUrl() {
+    return (
+        <div>
+            <p>Edit url</p>
+        </div>
+    );
+}
+
+export default EditUrl;
---
+++ src/UrlList/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function UrlList() {
+    return (
+        <div>
+            <p>Url List</p>
+        </div>
+    );
+}
+
+export default UrlList;
